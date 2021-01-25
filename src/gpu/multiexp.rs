342443177a11619378900eded64094495e888f7a
Bo Lin <coinmini@gmail.com>
2021-01-26 00:49:36 +0800

    change like 2080Ti

--- src/gpu/multiexp.rs
+++ src/gpu/multiexp.rs
@@ -114,7 +114,7 @@ where
         // let max_n = calc_chunk_size::<E>(mem, core_count);
         // let best_n = calc_best_chunk_size(MAX_WINDOW_SIZE, core_count, exp_bits);
         // let n = std::cmp::min(max_n, best_n);
-        let n = 20000000;
+        let n = 30000000;
 
         Ok(SingleMultiexpKernel {
             program: opencl::Program::from_opencl(d, &src)?,
@@ -165,8 +165,8 @@ where
         let size3 = std::mem::size_of::<<G as CurveAffine>::Projective>();
         let mem1 = size1 * n;
         let mem2 = size2 * n;
-        let mem3 = size3 * 4 * self.core_count * bucket_len;
-        let mem4 = size3 * 4 * self.core_count;
+        let mem3 = size3 * 2 * self.core_count * bucket_len;
+        let mem4 = size3 * 2 * self.core_count;
         info!("GABEDEBUG: <G> size:{}, <PrimeField> size:{}, <Projective> size:{}", size1, size2, size3);
         info!("GABEDEBUG: GPU mem need:{}byte, {}Mbyte", mem1 + mem2 + mem3 + mem4, (mem1 + mem2 + mem3 + mem4)/(1024*1024));
         
@@ -178,7 +178,7 @@ where
 
 
 
-
+//这里的参数2 不能调成4，不然内存完全不够
         let mut base_buffer = self.program.create_buffer::<G>(n)?;
         base_buffer.write_from(0, bases)?;
         let mut exp_buffer = self
@@ -320,7 +320,7 @@ where
         let (cpu_bases, bases) = bases.split_at(cpu_n);
         let (cpu_exps, exps) = exps.split_at(cpu_n);
         // let chunk_size = ((n as f64) / (num_devices as f64)).ceil() as usize;
-        let chunk_size = 20000000;
+        let chunk_size = 30000000;
 
         crate::multicore::THREAD_POOL.install(|| {
             use rayon::prelude::*;
@@ -341,7 +341,7 @@ where
                             .zip(self.kernels.par_iter_mut())
                             .map(|((bases, exps), kern)| -> Result<<G as CurveAffine>::Projective, GPUError> {
                                 let mut acc = <G as CurveAffine>::Projective::zero();
-                                let jack_chunk_3080 = 20000000;
+                                let jack_chunk_3080 = 30000000;
                                 let mut jack_windows_size = 11;
                                 let size_result = std::mem::size_of::<<G as CurveAffine>::Projective>();
                                 if size_result > 144 {
